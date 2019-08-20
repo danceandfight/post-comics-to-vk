12585dd38c9b605a8036d210c14e0523cc74f498
Pavel Zotikov <pavelzotikov@MacBook-Pro-Pavel.local>
2019-08-20 21:23:07 +0300

    Update main.py

--- post-comics-to-vk/main.py
+++ post-comics-to-vk/main.py
@@ -4,21 +4,21 @@ import random
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
-def check_response_for_error(response, response_json):
+def check_vk_response_for_error(response, response_json):
     if not response.ok or 'error' in response_json:
         raise requests.exceptions.HTTPError(response['error'])
 
 def get_random_comics_params():
     current_comics_url = 'http://xkcd.com/info.0.json'
     response = requests.get(current_comics_url)
+    response.raise_for_status()
     response_data = response.json()
-    check_response_for_error(response, response_data)
     comics_total_number = response_data['num']
     random_comics_number = random.randrange(1, comics_total_number + 1)
     random_comics_url = 'http://xkcd.com/{}/info.0.json'.format(random_comics_number)
     response = requests.get(random_comics_url)
+    response.raise_for_status()
     response_data = response.json()
-    check_response_for_error(response, response_data)
     comics_url = response_data['img']
     comment = response_data['alt']
     url_path = urlparse(comics_url)
@@ -27,8 +27,7 @@ def get_random_comics_params():
   
 def download_comics(url, filename):
     response = requests.get(url)
-    if not response.ok:
-        raise requests.exceptions.HTTPError(response['error'])
+    response.raise_for_status()
     os.makedirs(os.path.abspath('temp_pics'), exist_ok = True)
     file_with_path = 'temp_pics/{}'.format(filename)
     with open(file_with_path, 'wb') as file:
@@ -43,7 +42,7 @@ def upload_comics(url, comment, filename):
     wall_upload_server_url = 'https://api.vk.com/method/photos.getWallUploadServer'
     response = requests.get(wall_upload_server_url, params=payload)
     response_data = response.json()
-    check_response_for_error(response, response_data)
+    check_vk_response_for_error(response, response_data)
     upload_url = response_data['response']['upload_url']
     album_id = response_data['response']['album_id']
     user_id = response_data['response']['user_id']
@@ -52,7 +51,7 @@ def upload_comics(url, comment, filename):
     with open(photo_with_path, 'rb') as photo:
         response = requests.post(upload_url, files={'photo': photo})
         response_data = response.json()
-        check_response_for_error(response, response_data)
+        check_vk_response_for_error(response, response_data)
         photo.close()
 
     data = {'server': response_data['server'], 
@@ -64,7 +63,7 @@ def upload_comics(url, comment, filename):
     album_url = 'https://api.vk.com/method/photos.saveWallPhoto'
     response = requests.post(album_url, data=data)
     response_data = response.json()
-    check_response_for_error(response, response_data)
+    check_vk_response_for_error(response, response_data)
     response_album_url = response_data['response'][0]
     media_id = response_album_url['id']
     owner_id = response_album_url['owner_id']
@@ -79,7 +78,7 @@ def upload_comics(url, comment, filename):
             'v': api_version}
     wall_post_url = 'https://api.vk.com/method/wall.post'
     response = requests.post(wall_post_url, data=params)
-    check_response_for_error(response, response.json())
+    check_vk_response_for_error(response, response.json())
 
 def main():
     load_dotenv()
